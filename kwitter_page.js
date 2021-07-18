1574489ba3d37dcb61aa3fda8f185f1d46553f11
Saanvi-cool <84757010+Saanvi-cool@users.noreply.github.com>
2021-07-18 14:23:22 -0400

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,48 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCAheWkrjZxapEMljpHXr9ohfQce8sZXKc",
+    authDomain: "twitter-8e621.firebaseapp.com",
+    projectId: "twitter-8e621",
+    storageBucket: "twitter-8e621.appspot.com",
+    messagingSenderId: "563818798460",
+    appId: "1:563818798460:web:94399c5b57aa22845a58f9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+username = localStorage.getItem("username_key");
+room_name = localStorage.getItem("room_name");
+
+function getData() {
+      firebase.database().ref("/" + room_name).on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  childData = childSnapshot.val();
+                  if (childKey != "purpose") {
+                        firebase_message_id = childKey;
+                        message_data = childData;
+                        //Start code
+console.log(message_data);
+                        //End code
+                  }
+            });
+      });
+}
+getData();
+
+function logout() {
+      localStorage.removeItem("username_key");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
+
+function send() {
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name: username,
+            message: msg,
+            like: 0 
+      });
+      document.getElementById("msg").value="";
+}
